b5a7e94c025fe1c46a3b3930f4859862b6016dca
kaxrthik <karthikkaranth222@gmail.com>
2025-03-24 05:26:33 +0530

    Corrected Go_back_N

--- CCN_lab/Expt2/go_back_n.c
+++ CCN_lab/Expt2/go_back_n.c
@@ -8,24 +8,27 @@
 int window_size, total_frames;
 
 // Function to simulate sending a frame
-void send_frame(int frame_number) {
+void send_frame(int frame_number) 
+{
     printf("Sent Frame %d\n", frame_number);
 }
 
 // Function to simulate receiving an acknowledgment
-int receive_ack(int frame_number) {
+int receive_ack(int frame_number)
+{
     return (rand() % 10) < 7; // 70% chance of successful ACK
 }
 
 // Function to simulate frame loss
-int frame_lost(int frame_number) {
+int frame_lost(int frame_number) 
+{
     return (rand() % 10) < 2; // 20% chance of frame loss
 }
 
-int main() {
-    int base = 0;
-    int next_frame = 0;
-    int ack_received;
+int main() 
+{
+    int base = 0;           // Base of the sliding window
+    int next_frame = 0;     // Next frame to send
 
     printf("Enter total frames to send: ");
     scanf("%d", &total_frames);
@@ -35,34 +38,37 @@ int main() {
 
     srand(time(NULL));
 
-    while (base < total_frames) {
-        while (next_frame < base + window_size && next_frame < total_frames) {
-            if (frame_lost(next_frame)) {
-                printf("Frame %d Lost. Resending from Frame %d...\n", next_frame, base);
-                next_frame = base; // Reset next_frame and resend window
-                break;
-            } else {
-                send_frame(next_frame);
-                next_frame++;
-            }
+    while (base < total_frames) 
+    {
+        // Send frames within the window
+        while (next_frame < base + window_size && next_frame < total_frames) 
+        {
+            send_frame(next_frame);
+            next_frame++;
         }
 
-        if(next_frame > base) // only wait for ack if frames were sent.
-        {
-          sleep(2);
+        // Wait for acknowledgments
+        sleep(2);
 
-          ack_received = 0;
-          for (int i = base; i < next_frame; i++) {
-              if (receive_ack(i)) {
-                  printf("ACK received for Frame %d\n", i);
-                  ack_received = 1;
-                  base++;
-              } else {
-                  printf("ACK lost for Frame %d. Resending from Frame %d...\n", i, base);
-                  next_frame = base;
-                  break;
-              }
-          }
+        while (base < next_frame) 
+        {
+            if (frame_lost(base)) 
+            {
+                printf("Frame %d lost. Resending window from Frame %d...\n", base, base);
+                next_frame = base; // Reset next_frame to resend from the base
+                break;
+            } 
+            else if (receive_ack(base)) 
+            {
+                printf("ACK received for Frame %d\n", base);
+                base++; // Move the window forward
+            } 
+            else 
+            {
+                printf("ACK lost for Frame %d. Resending from Frame %d...\n", base, base);
+                next_frame = base; // Resend from the base
+                break;
+            }
         }
     }
 
