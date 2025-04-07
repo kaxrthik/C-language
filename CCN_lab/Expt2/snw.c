257506c4f49f8bebffa2a67f12c23bbcfe2e9b4a
kaxrthik <karthikkaranth222@gmail.com>
2025-04-07 13:25:11 +0530

    DONE

--- CCN_lab/Expt2/snw.c
+++ CCN_lab/Expt2/snw.c
@@ -5,16 +5,19 @@
 
 void send_frame(int frame_num)
 {
-    printf("Transmitting Frame %d... \n", frame_num);
+    printf("Transmitted Frame %d... \n", frame_num);
 }
-int wait_for_ACK(int frame_num)
+
+// Simulate transmission outcome
+// 0 = Success, 1 = ACK lost, 2 = Frame lost
+int simulate_outcome()
 {
-    return rand() % 2;
+    return rand() % 3;
 }
 
 int main()
 {
-    int total_frames, timeout,rtt;
+    int total_frames, timeout, rtt;
     printf("Enter the number of frames to send: ");
     scanf("%d", &total_frames);
     printf("Enter round trip time (in seconds): ");
@@ -28,28 +31,39 @@ int main()
     while(Frame < total_frames)
     {
         send_frame(Frame);
+        int outcome = simulate_outcome(); // Decide what happens AFTER sending
         int ack = 0, wait_time = 0;
-        while(wait_time < timeout)
+
+        if (outcome == 2) // Frame lost
         {
-            sleep(1);
-            wait_time++;
-            if(wait_for_ACK(Frame))
+            printf("Frame %d was lost during transmission.\n", Frame);
+        }
+        else
+        {
+            while (wait_time < timeout)
             {
-                printf("ACK received for frame %d \n", Frame);
-                ack = 1;
-                break;
-            }
-            else
-            {
-                printf("Waiting for ACK on frame %d... \n", Frame);
-            }
-            if(wait_time>rtt)
-            {
-                printf("Round trip time limit reached for frame %d\n", Frame);
-                break;
+                sleep(1);
+                wait_time++;
+
+                if (outcome == 1) // ACK lost
+                {
+                    printf("Waiting for ACK on frame %d... \n", Frame);
+                    if (wait_time == rtt)
+                    {
+                        printf("ACK lost for Frame %d.\n", Frame);
+                        break;
+                    }
+                }
+                else if (outcome == 0) // Successful ACK
+                {
+                    printf("ACK received for frame %d \n", Frame);
+                    ack = 1;
+                    break;
+                }
             }
         }
-        if(!ack)
+
+        if (!ack)
         {
             printf("Timeout! Retransmitting Frame %d...\n", Frame);
         }
@@ -58,6 +72,7 @@ int main()
             Frame++;
         }
     }
+
     printf("All frames transmitted successfully.\n");
     return 0;
 }
