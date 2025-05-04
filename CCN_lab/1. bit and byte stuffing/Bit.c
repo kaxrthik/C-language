1766ec4eca9930cdbfddc178a6aa763127cd5fb3
kaxrthik <karthikkaranth222@gmail.com>
2025-05-04 13:14:58 +0530

    I hate my Life

--- CCN_lab/1. bit and byte stuffing/Bit.c
+++ CCN_lab/1. bit and byte stuffing/Bit.c
@@ -1,1 +1,21 @@
-#include <yourMOM.h>
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i,j = 0;
+    char frame[100], stufframe[200];
+    printf("Enter input frame (0's & 1's only): ");
+    scanf("%s", frame);
+    strcat(stufframe, "01111110 ");
+    for (i = 0; frame[i]; i++)
+    {
+        stufframe[j++] = frame[i];
+        if (i >= 5 && frame[i - 5] == '0' && frame[i - 4] == '1' && frame[i - 3] == '1' && frame[i - 2] == '1' && frame[i - 1] == '1' && frame[i] == '1')
+        {
+            stufframe[j++] = '0';
+        }
+    }
+    strcat(stufframe, " 01111110");
+    printf("After Bit Stuffing: %s\n", stufframe);
+    return 0;
+}
--- CCN_lab/1. bit and byte stuffing/Byte.c
+++ CCN_lab/1. bit and byte stuffing/Byte.c
@@ -1,1 +1,29 @@
-#include <yourMOM.h>
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int i = 0, j = 0;
+    char frame[100], stuffframe[200] = "\0";
+    char beginningFlag[] = "dlestx";
+    char endFlag[] = "dleetx";
+
+    printf("Enter frame: "); // Prompt the user to enter a frame
+    scanf("%s", frame);
+    strcpy(stuffframe, beginningFlag); // Start the stuffed frame with the starting flag
+    j = strlen(stuffframe);
+
+    while (frame[i] != '\0')
+    {
+        if (frame[i] == 'd' && frame[i + 1] == 'l' && frame[i + 2] == 'e') // Check if a 'dle' sequence starts here
+        {
+            // Stuff another 'dle' before the actual 'dle'
+            stuffframe[j++] = 'd';
+            stuffframe[j++] = 'l';
+            stuffframe[j++] = 'e';
+        }
+        stuffframe[j++] = frame[i++];
+    }
+    strcat(stuffframe, endFlag);
+    printf("\nFrame after stuffing: %s\n", stuffframe);
+    return 0;
+}
